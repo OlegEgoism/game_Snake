49a1bff4e12422d3dcf220cd38a59bfe6f36cc81
Oleg <olegpustovalov220@gmail.com>
2024-01-02 00:30:51 +0300

    Новая версия

--- Snake.py
+++ Snake.py
@@ -2,7 +2,7 @@ import pygame
 from random import randrange
 
 game_width, game_height = 400, 600  # Ширина, Высота экрана
-size = 40  # Толщина
+size = 20  # Размер
 x, y = randrange(size, game_width - size, size), randrange(size, game_height - size, size)  # Начальное случайное положение змеи
 apple = randrange(size, game_width - size, size), randrange(size, game_height - size, size)  # Начальное случайное положение яблок
 length = 1  # Длина змейки
@@ -24,11 +24,15 @@ def close_game():
         if event.type == pygame.QUIT:
             exit()
 
-
 while True:
-    surface.blit(img, (0, 0))  # Сам рисунок
-    [pygame.draw.rect(surface, pygame.Color('green'), (i, j, size, size)) for i, j in snake]  # Размер и цвет кубика
-    pygame.draw.rect(surface, pygame.Color('yellow'), (*apple, size, size))  # Размер и цвет змейки
+    surface.blit(img, (0, 0))
+    for i, j in snake:
+        # Рисуем границу вокруг каждого сегмента змеи
+        pygame.draw.rect(surface, pygame.Color('black'), (i, j, size, size))  # Обводка
+        pygame.draw.rect(surface, pygame.Color('green'), (i + 2, j + 2, size - 4, size - 4))
+    # pygame.draw.rect(surface, pygame.Color('yellow'), (*apple, size, size))
+    pygame.draw.circle(surface, pygame.Color('yellow'), (apple[0] + size // 2, apple[1] + size // 2), size // 2) # Цвет кубика
+
     render_score = font_score.render(f'Очки: {score}', 0, pygame.Color('royalblue'))  # Надпись Очки цвет
     surface.blit(render_score, (6, 2))  # Надпись очки расположение
     speed_count += 1  # Движение змейки
@@ -39,8 +43,7 @@ while True:
         snake = snake[-length:]
 
     if snake[-1] == apple:
-        apple = randrange(size, game_width - size, size), randrange(size, game_height - size,
-                                                                    size)  # Нахождение обьекта
+        apple = randrange(size, game_width - size, size), randrange(size, game_height - size, size)  # Нахождение обьекта
         length += 1
         score += 1
         snake_speed -= 1
@@ -74,3 +77,19 @@ while True:
         if dirs['D']:
             dx, dy = 1, 0
             dirs = {'W': True, 'S': True, 'A': False, 'D': True, }
+    elif key[pygame.K_UP]:
+        if dirs['W']:
+            dx, dy = 0, -1
+            dirs = {'W': True, 'S': False, 'A': True, 'D': True}
+    elif key[pygame.K_DOWN]:
+        if dirs['S']:
+            dx, dy = 0, 1
+            dirs = {'W': False, 'S': True, 'A': True, 'D': True}
+    elif key[pygame.K_LEFT]:
+        if dirs['A']:
+            dx, dy = -1, 0
+            dirs = {'W': True, 'S': True, 'A': True, 'D': False}
+    elif key[pygame.K_RIGHT]:
+        if dirs['D']:
+            dx, dy = 1, 0
+            dirs = {'W': True, 'S': True, 'A': False, 'D': True}
